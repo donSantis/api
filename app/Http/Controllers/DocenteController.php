6928ef76fe8e945a4d156dd0e75fd3bbe0f4709f
dimitryjacques <di.jacques@duocuc.cl>
2022-05-17 22:42:26 -0400

    view perfil y docente

---
+++ app/Http/Controllers/DocenteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Http\Requests\StoreCommentRequest;
+use App\Http\Requests\UpdateCommentRequest;
+
+class DocenteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('docente.index');
+    }
+
+
+
+
+}
